e28dc5a1cddae921dbc66db0167ef27be4d80d2d
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2019-11-08 23:00:43 -0500

    clean up error handling

--- src/main.rs
+++ src/main.rs
@@ -8,50 +8,29 @@ mod pinout;
 use pinout::*;
 
 fn main() {
-    let maybe_input_pin_1 = initialize_gpio_pin(pinout::RPI_GPIO_INT_PIN).ok();
-    let maybe_device = initialize_i2c_device(pinout::I2C_EXPANDER_1).ok();
-    
-    let mut device = maybe_device.unwrap();
-    let input_pin_state = maybe_input_pin_1.unwrap();
+    let gpio = Gpio::new().expect("A new GPIO should have been created");
+    let maybe_input_pin_1 = gpio.get(RPI_GPIO_INT_PIN).expect("A new interrupt pin should be created");
+
+    let mut i2c_device_1 = LinuxI2CDevice::new("/dev/i2c-1", pinout::I2C_EXPANDER_1).expect("A new i2c device should have been created");
+
+    initialize_i2c_device(&mut i2c_device_1).expect("An i2c should have been initialized");
+
+    let input_pin_state = maybe_input_pin_1.into_input_pulldown();
 
     loop{
         if input_pin_state.is_high(){
-            read_i2c(&mut device, pinout::GPIO_A);
+            read_i2c(&mut i2c_device_1, pinout::GPIO_A);
         }
     }
 }
 
-fn initialize_gpio_pin(pin_number: u8) -> Result<InputPin, ()>{
-    let mut gpio = Gpio::new().ok();
-    let mut maybe_pin = gpio.unwrap().get(pin_number);
-    match maybe_pin{
-        Ok(pin) => {
-            let input_pin = pin.into_input_pulldown();
-            Ok(input_pin)
-        }
-
-        Err(shit) => {
-                eprintln!("{:?}", shit);
-                println!("shit went down");
-                Err(())
-        }
-
-    }
-    
-}
-
-fn initialize_i2c_device(dev_address: u16) -> Result<LinuxI2CDevice, LinuxI2CError>{
-    let mut dev = LinuxI2CDevice::new("/dev/i2c-1", dev_address)?;
+fn initialize_i2c_device(dev: &mut LinuxI2CDevice) -> Result<(), LinuxI2CError>{
     dev.smbus_write_byte_data(pinout::IODIRA, 0xff)?;
-
     dev.smbus_write_byte_data(pinout::DEFVALA, 0x00)?;
     dev.smbus_write_byte_data(pinout::DEFVALB, 0x00)?;
-
     dev.smbus_write_byte_data(pinout::INTCONA, 0xff)?;
-
     dev.smbus_write_byte_data(pinout::GPINTAEN, 0xff)?;
-
-    Ok(dev)
+    Ok(())
 }
 
 fn read_i2c(dev: &mut LinuxI2CDevice, register: u8) -> Result<(), LinuxI2CError>{
@@ -60,7 +39,10 @@ fn read_i2c(dev: &mut LinuxI2CDevice, register: u8) -> Result<(), LinuxI2CError>
     match dev.smbus_read_byte_data(register){
         Ok(state) => {
             match state {
-                1 => state_bool = true,
+                1 => {
+                    state_bool = true;
+                    dev.smbus_write_byte_data(pinout::GPIO_B, 0xff);
+                },
                 0 => state_bool = false,
                 _ => {
                         println!("shit went down and also {}", state);
