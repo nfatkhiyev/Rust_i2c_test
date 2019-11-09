6d8eb18c47454254132beb955b526de64ff51ead
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2019-11-08 22:08:28 -0500

    check interrupt pin on RPi first

---
+++ src/pinout.rs
@@ -0,0 +1,19 @@
+
+pub const I2C_EXPANDER_1: u16 = 0x20;
+
+pub const GPINTAEN: u8 = 0x04;
+pub const GPINTBEN: u8 = 0x05;
+
+pub const DEFVALA: u8 = 0x06;
+pub const DEFVALB: u8 = 0x07;
+
+pub const INTFA: u8 = 0x0e;
+pub const INTCAPA: u8 = 0x10;
+pub const INTCONA: u8 = 0x08;
+
+pub const GPIO_A: u8 = 0x12;
+pub const GPIO_B: u8 = 0x13;
+
+pub const IODIRA: u8 = 0x00;
+
+pub const RPI_GPIO_INT_PIN: u8 = 22;
